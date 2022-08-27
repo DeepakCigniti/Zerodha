cb1ea21303b7c491bedc2c5ae17000eb00cccc47
Deepak <Deepak@ADMINRG-E9DFN3E>
2022-08-27 19:38:17 +0530

    Hello

---
+++ src/test/java/Deepak/D.java
@@ -0,0 +1,14 @@
+package Deepak;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class D {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.cricbuzz.com/");
+}
+}
